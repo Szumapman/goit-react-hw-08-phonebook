5477bc2733bfbdeceba9f3e5a119b96f7e2c81c5
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-12 16:08:13 +0100

    add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import BASE_PATH from './constants/BasePath.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename={BASE_PATH}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
