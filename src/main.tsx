5958d386c5fb0aae175d8cf93a8eebd68a932947
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 20:44:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import BASE_PATH from './constants/BasePath.ts'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App.tsx';
+import BASE_PATH from './constants/BasePath.ts';
+import { system } from '@chakra-ui/react/preset';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter basename={BASE_PATH}>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ChakraProvider value={system}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename={BASE_PATH}>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
   </StrictMode>,
 )
