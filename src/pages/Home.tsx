a75629f781fcd58c20b6f4be7c52a3fdfdaace7c
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 20:44:00 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
+import useAuth from "../hooks/useAuth";
+import { Flex, Heading, HStack, Link, Text } from "@chakra-ui/react"
+import { GrLogout } from "react-icons/gr";
+import { MdOutlineContacts } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+    const { isLoggedIn } = useAuth();
+    const navigate = useNavigate();
     return (
         <>
             <title>Home</title>
-            <h1>Phonebook</h1>
+            <Flex h="80vh" alignItems="center" justifyContent="start" marginTop={20} flexDirection={"column"} gap={4}>
+                <HStack gap={4} >
+                    <MdOutlineContacts size={"4rem"} color="#2196F3" />
+                    <Heading>
+                        Welcome to Phonebook 
+                    </Heading>
+                </HStack>
+                <Text>Manage your contacts efficiently!</Text>
+                {isLoggedIn && <Link onClick={() => { navigate("/logout") }} color={"blue.600"}>
+                    <GrLogout /> Logout
+                </Link>}
+                {!isLoggedIn && <Text><Link onClick={() => { navigate("/register") }} color={"blue.600"}>Register</Link> or <Link onClick={() => { navigate("/login") }} color={"blue.600"}>log in</Link> to get started.</Text>}
+            </Flex>
         </>
     );
 };
