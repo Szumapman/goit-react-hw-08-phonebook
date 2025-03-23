2a69f3d34c296bc0c12806a6a5492f99d3f46daf
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 20:43:30 +0100

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contactsSlice";
-import filterReducer from "./filterSlice";
+import contactsReducer from "./contacts/contactsSlice";
+import filterReducer from "./contacts/filterSlice";
+import { authReducer } from "./auth/authSlice";
+import { persistReducer, persistStore, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+const authPersistConfig = {
+    key: "auth",
+    storage,
+    whitelist: ["token"],
+};
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filter: filterReducer,
+        auth: persistReducer<any>(authPersistConfig, authReducer),
     },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
