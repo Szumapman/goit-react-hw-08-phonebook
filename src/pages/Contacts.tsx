fd2974b0b0381280dc84ba89cf61fa2079e11b41
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 21:38:34 +0100

    Update Contacts.tsx

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { fetchContacts } from "../redux/contacts/operations";
 import { Filter } from "../components/Filter";
 import { ContactList } from "../components/ContactList";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AddContact from "@/components/AddContact";
 import { Flex } from "@chakra-ui/react";
+import { selectContacts } from "@/redux/contacts/selectors";
+import { Contact } from "@/types/Contact";
 
 const Contacts = () => {
     const dispatch = useDispatch();
+    const contacts: Contact[] = useSelector(selectContacts);
     
 
     useEffect(() => {
@@ -19,7 +22,7 @@ const Contacts = () => {
             <title>Contacts</title>
             <Flex flexDirection="column" gap={4} alignItems="center" justifyContent="center" marginTop={20}>
                 <AddContact />
-                <Filter />
+                {contacts.length > 0 && <Filter />}
                 <ContactList />
             </Flex>
         </>
