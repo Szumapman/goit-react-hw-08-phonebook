a8eb5c2c5be283420af8253e3c88fd6d942df03e
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 20:41:53 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useDispatch, useSelector } from 'react-redux';
-import './App.css'
-import { ContactForm } from './components/ContactForm/ContactForm'
-import { ContactList } from './components/ContactList/ContactList';
-import { Filter } from './components/Filter/Filter';
-import { selectError, selectIsLoading } from './redux/selectors';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout';
+import Home from './pages/Home';
+import Contacts from './pages/Contacts';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import PrivateRoute from './components/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchContacts } from './redux/operations';
+import { refreshUser } from './redux/auth/operations';
+import useAuth from './hooks/useAuth';
+import Loader from './components/Loader';
 
 
 function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError)
-
-  useEffect(() => {
-    (dispatch as any)(fetchContacts());
-  }, [dispatch]);
+  const { isRefreshing } = useAuth();
   
+  useEffect(() => {
+    (dispatch as any)(refreshUser());
+  }, []);
 
+  if (isRefreshing) {
+    return (
+      <Loader />
+    )
+  }
   return (
-    <>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {!isLoading && !error && <ContactList />}
-    </>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route
+          path='/contacts' element={
+            <PrivateRoute component={<Contacts />} redirectTo="/login" />
+          }
+        />
+        <Route path='/register' element={
+            <RestrictedRoute component={<Register />} redirectTo="/contacts" />
+          }
+        />
+        <Route path='/login' element={
+            <RestrictedRoute component={<Login />} redirectTo="/contacts" />
+          }
+        /> 
+      </Route>
+    </Routes>
   )
 }
 
