85368fa253e84a61c0c222157d7c195803432374
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 20:43:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/goit-react-hw-08-phonebook/',
-  plugins: [react()],
+  plugins: [react(),  tsconfigPaths()],
 })
