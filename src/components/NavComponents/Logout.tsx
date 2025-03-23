7306a7317a343b66d96ea9fa97a2985b0b90b48e
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-23 21:02:02 +0100

    Update Logout.tsx

--- src/components/NavComponents/Logout.tsx
+++ src/components/NavComponents/Logout.tsx
@@ -2,12 +2,15 @@ import { useDispatch } from "react-redux";
 import { logOut } from "../../redux/auth/operations";
 import { HStack, Link } from "@chakra-ui/react";
 import { GrLogout } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
-        (dispatch as any)(logOut()) 
+        (dispatch as any)(logOut())
+        navigate("/login");
     };
     return (
         <HStack >
