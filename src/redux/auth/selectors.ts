d9ea918514dfad4dd053add436926c380cc40ba3
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-28 15:59:29 +0100

    Update selectors.ts

--- src/redux/auth/selectors.ts
+++ src/redux/auth/selectors.ts
@@ -1,5 +1,7 @@
 const selectIsLoggedIn = (state: any) => state.auth.isLoggedIn;
 const selectUser = (state: any) => state.auth.user;
 const selsectIsRefreshing = (state: any) => state.auth.isRefreshing;
+const selectError = (state: any) => state.auth.error;
 
-export { selectIsLoggedIn, selectUser, selsectIsRefreshing };
+
+export { selectIsLoggedIn, selectUser, selsectIsRefreshing, selectError };
